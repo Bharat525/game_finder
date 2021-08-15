d2517c7a86b6c4282d3fe2f9f5802aae1d15690a
Bharat525 <see.rohit525@gmial.com>
2021-08-15 21:25:27 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
 
 function App() {
+  const [data , setData] = useState(null);
+ 
+
+
+  const [input , setinput] = useState('');
+
+  useEffect(()=>{
+    axios.get('http://ergast.com/api/f1/2004/1/results.json')
+    .then((data) =>{
+      setData(data.data);
+      console.log(data.data.MRData.RaceTable.Races[0].ircuit)
+     
+
+
+    })
+
+    .catch((error) => console.log(error))
+
+
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
     </div>
   );
 }
